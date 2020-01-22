e4c9c74b9f852f62c7d4eb11ada92b37724c718c
orcun <orcunkamiloglu@gmail.com>
2020-01-22 16:00:02 +0300

    Voice recognition

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/voice/VoiceRecognitionActivity.java
@@ -0,0 +1,14 @@
+package org.tensorflow.lite.examples.detection.voice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoiceRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recognition);
+    }
+}
